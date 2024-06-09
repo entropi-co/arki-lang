3e80d77e6abff0a549c5b325185ba654168b5e62
arcranion <devlaq2331@gmail.com>
2024-06-09 23:56:02 +0900

    Update to data class and Added priority, locale

--- core/src/main/kotlin/kr/entropi/arki/lang/ArkiLangFile.kt
+++ core/src/main/kotlin/kr/entropi/arki/lang/ArkiLangFile.kt
@@ -1,12 +1,18 @@
 package kr.entropi.arki.lang
 
-open class ArkiLangFile(
+import java.util.Locale
+
+data class ArkiLangFile(
     val inherit: ArkiLangFile? = null,
-        val values: MutableMap<String, String> = mutableMapOf()
+    val priority: Int = 0,
+    val locale: Locale = Locale.getDefault(),
+    val data: MutableMap<String, String> = mutableMapOf()
 ) {
     fun resolve(key: String): String? {
-        return values[key] ?: inherit?.resolve(key)
+        return data[key] ?: inherit?.resolve(key)
     }
 
-    object Empty: ArkiLangFile()
+    companion object {
+        val Empty = ArkiLangFile()
+    }
 }
