7f9b516bc83bc5f6647cdd306d29bb878a67bc91
arcranion <devlaq2331@gmail.com>
2024-06-09 19:21:42 +0900

    Add missing build.gradle.kts

---
+++ paper/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    kotlin("jvm") version "1.9.23"
+
+    id("io.papermc.paperweight.userdev") version "1.7.1"
+}
+
+group = "kr.entropi.arki.lang"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
+    maven("https://oss.sonatype.org/content/repositories/snapshots")
+    maven("https://oss.sonatype.org/content/repositories/central")
+    maven("https://jitpack.io")
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation(project(":core"))
+
+    // Paper
+    paperweight.paperDevBundle("1.20.4-R0.1-SNAPSHOT")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
