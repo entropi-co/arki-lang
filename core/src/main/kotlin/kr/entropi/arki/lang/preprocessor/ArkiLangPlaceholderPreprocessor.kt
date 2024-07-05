4cf5b8a08cd556b180433f32d7d109f93e03edbe
arcranion <devlaq2331@gmail.com>
2024-07-05 23:16:38 +0900

    Add iteration for recursive resolving placeholders

--- core/src/main/kotlin/kr/entropi/arki/lang/preprocessor/ArkiLangPlaceholderPreprocessor.kt
+++ core/src/main/kotlin/kr/entropi/arki/lang/preprocessor/ArkiLangPlaceholderPreprocessor.kt
@@ -2,13 +2,18 @@ package kr.entropi.arki.lang.preprocessor
 
 import kr.entropi.arki.lang.ArkiLangFile
 import kr.entropi.arki.lang.ArkiLangPreprocessor
+import org.slf4j.LoggerFactory
 
-class ArkiLangPlaceholderPreprocessor(
+class ArkiLangPlaceholderPreprocessor : ArkiLangPreprocessor() {
+    private val logger = LoggerFactory.getLogger(ArkiLangPlaceholderPreprocessor::class.java)
+
     override var next: ArkiLangPreprocessor? = null
-) : ArkiLangPreprocessor() {
+
     companion object {
-        val placeholderPattern = Regex("""(?<!\\)\$\{(\w+)}""")
+        val placeholderPattern = Regex("""(?<!\\)\$\{([_.a-zA-Z0-9]+)}""")
         val escapePattern = Regex("""\\\$\{(\w+)}""")
+
+        const val MAX_MATCH_ITERATIONS = 500
     }
 
     override fun preprocess(file: ArkiLangFile): ArkiLangFile {
@@ -19,20 +24,31 @@ class ArkiLangPlaceholderPreprocessor(
             var newValue = value
 
             // Replace placeholders with their corresponding values
-            placeholderPattern.findAll(value).forEach { matchResult ->
-                val referenceName = matchResult.groupValues[1]
-                if (originMap.containsKey(referenceName)) {
-                    val referenceValue = originMap[referenceName]
-                    newValue = newValue.replace(matchResult.value, referenceValue ?: "")
+            var iter = 0
+            while(true) {
+                if(iter > MAX_MATCH_ITERATIONS) {
+                    logger.warn("Match iteration exceed the limit ${MAX_MATCH_ITERATIONS}, causes performance issues. The language file may be wrong")
+                    break
                 }
-            }
+                iter++
 
-            // Unescape the escaped placeholders without replacing them
-            newValue = escapePattern.replace(newValue) { matchResult ->
-                matchResult.value.drop(1) // Remove only the escape character
-            }
+                val matchResults = placeholderPattern.findAll(newValue)
+                if(!matchResults.iterator().hasNext()) break
+                matchResults.forEach { matchResult ->
+                    val referenceName = matchResult.groupValues[1]
+                    if (originMap.containsKey(referenceName)) {
+                        val referenceValue = originMap[referenceName]
+                        newValue = newValue.replace(matchResult.value, referenceValue ?: "")
+                    }
+                }
 
-            processingMap[key] = newValue
+                // Unescape the escaped placeholders without replacing them
+                newValue = escapePattern.replace(newValue) { matchResult ->
+                    matchResult.value.drop(1) // Remove only the escape character
+                }
+
+                processingMap[key] = newValue
+            }
         }
 
         val processedFile = file.copy(
