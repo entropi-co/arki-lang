b2aecc925a512aa794ee4e28a04f7811c430ef0a
arcranion <devlaq2331@gmail.com>
2024-07-05 23:18:33 +0900

    Configure publish to GitHub Packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.9.23"
+    `maven-publish`
 }
 
 group = "kr.entropi.arki.lang"
@@ -7,6 +8,7 @@ version = "1.0-SNAPSHOT"
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "maven-publish")
 
     repositories {
         mavenLocal()
@@ -15,5 +17,24 @@ allprojects {
 
     dependencies {
         compileOnly("org.slf4j:slf4j-api:2.0.13")
+    }
+
+    configure<PublishingExtension> {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/entropi-kr/arki-lang")
+                credentials {
+                    username = System.getenv("GITHUB_ACTOR")
+                    password = System.getenv("GITHUB_TOKEN")
+                }
+            }
+        }
+
+        publications {
+            register<MavenPublication>("mavenKt") {
+                from(components["java"])
+            }
+        }
     }
 }
